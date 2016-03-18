217bd9efca271b6512f3443609ae02ec0793d813
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-03-18 21:30:58 +0800

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by kersuzan on 17/03/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/Player.java
@@ -0,0 +1,11 @@
+/**
+ * Created by kersuzan on 17/03/16.
+ */
+public abstract class Player {
+
+
+
+    public Player() {
+
+    }
+}
